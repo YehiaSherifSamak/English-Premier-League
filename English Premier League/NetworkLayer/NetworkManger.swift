2892a346f3ca173304cc449ebf2e765dee182732
Yehia Samak <ysamak17@gmail.com>
2023-04-11 22:56:49 +0200

    [impelement] Network manger class

--- English Premier League/NetworkLayer/NetworkManger.swift
+++ English Premier League/NetworkLayer/NetworkManger.swift
@@ -5,8 +5,41 @@
 //  Created by Yehia Samak on 11/04/2023.
 //
 
+import Combine
 import Foundation
 
-class NetworkManger {
+public protocol Requestable {
+    var requestTimeOut: Float { get }
     
+    func request<T: Codable>(_ req: NetworkRequest) -> AnyPublisher<T, NetworkError>
 }
+
+class NetworkManger: Requestable {
+    public var requestTimeOut: Float = 30
+    
+    public func request<T>(_ req: NetworkRequest) -> AnyPublisher<T, NetworkError>
+    where T: Decodable, T: Encodable {
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.timeoutIntervalForRequest = TimeInterval(req.requestTimeOut ?? requestTimeOut)
+        
+        guard let url = URL(string: req.url) else {
+            return AnyPublisher(
+                Fail<T, NetworkError>(error: NetworkError.badURL("Invalid Url"))
+            )
+        }
+        return URLSession.shared
+            .dataTaskPublisher(for: req.buildURLRequest(with: url))
+            .tryMap { output in
+                guard output.response is HTTPURLResponse else {
+                    throw NetworkError.serverError(code: 0, error: "Server error")
+                }
+                return output.data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError { error in
+                NetworkError.invalidJSON(String(describing: error))
+            }
+            .eraseToAnyPublisher()
+    }
+}
+
